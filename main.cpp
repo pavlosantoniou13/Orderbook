5219bccbdffa6d9415450171b4a00eba4f681ad0
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-09-25 17:36:31 +0200

    OrderModify scope change

--- main.cpp
+++ main.cpp
@@ -98,27 +98,28 @@ using OrderPointers = std::list<OrderPointer>;
 
 class OrderModify
 {
-    OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
-        : orderId_{ orderId }
-        , price_{ price }
-        , side_{ side }
-        , quantity_{ quantity }
-        { }
+    public:
+        OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+            : orderId_{ orderId }
+            , price_{ price }
+            , side_{ side }
+            , quantity_{ quantity }
+            { }
 
-        OrderId getOrderId() const { return orderId_; }
-        Price getPrice() const { return price_; }
-        Side getSide() const { return side_; }
-        Quantity getQuantity() const { return quantity_ ;}
+            OrderId getOrderId() const { return orderId_; }
+            Price getPrice() const { return price_; }
+            Side getSide() const { return side_; }
+            Quantity getQuantity() const { return quantity_ ;}
 
-        OrderPointer ToOrderPointer(OrderType type) const 
-        {
-            return std::make_shared<Order>(type, getOrderId(), getSide(), getPrice(), getQuantity());
-        }
-    private:
-        OrderId orderId_;
-        Price price_;
-        Side side_;
-        Quantity quantity_;
+            OrderPointer ToOrderPointer(OrderType type) const 
+            {
+                return std::make_shared<Order>(type, getOrderId(), getSide(), getPrice(), getQuantity());
+            }
+        private:
+            OrderId orderId_;
+            Price price_;
+            Side side_;
+            Quantity quantity_;
 };
 
 struct TradeInfo
