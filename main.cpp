98a818c566cc69d1ccbd0f9825ffc7479c9d5fb9
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-09-25 13:38:45 +0200

    Add Trade class

--- main.cpp
+++ main.cpp
@@ -120,6 +120,33 @@ class OrderModify
         Quantity quantity_;
 };
 
+struct TradeInfo
+{
+    OrderId orderId;
+    Price price_;
+    Quantity quantity_;
+};
+
+class Trade
+{
+    public:
+        Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+        : bidTrade_{ bidTrade }
+        , askTrade_{ askTrade }
+        { }
+
+        const TradeInfo& getBidTrade() const { return bidTrade_; }
+        const TradeInfo& getAskTrade() const { return askTrade_; }
+
+    private:
+        TradeInfo bidTrade_;
+        TradeInfo askTrade_;
+};
+
+using Trades = std::vector<Trade>;
+
+
+
 
 
 int main() {
