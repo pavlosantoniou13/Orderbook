e3ad555dfd0b2be75c27b41cb4f6834f1c5788e7
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-09-25 12:00:17 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <limits>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include <memory>
+#include <variant>
+#include <optional>
+#include <tuple>
+#include <format>
+
+int main() {
+    
+    return 0;
+}
