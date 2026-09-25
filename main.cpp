20319fe3603f6712e4c81e274ac32f44a5219857
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-09-25 13:10:06 +0200

    Add Order class

--- main.cpp
+++ main.cpp
@@ -57,6 +57,43 @@ class OrderBookLevelInfos
         LevelInfos asks_;
 };
 
+class Order 
+{
+    public:
+        Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_{ orderType }
+        , orderId_{ orderId }
+        , side_{ side }
+        , price_{ price }
+        , initialQuantity_{ quantity }
+        , remainingQuantity_{ quantity }
+    { }
+    
+    OrderId getOrderId() const { return orderId_; }
+    Side getSide() const { return side_; }
+    Price getPrice() const { return price_; }
+    OrderType getOrderType() const { return orderType_; }
+    Quantity getInitialQuantity() const { return initialQuantity_; }
+    Quantity getRemainingQuantity() const { return remainingQuantity_; }
+    Quantity getFilledQuantity() const { return getInitialQuantity() - getRemainingQuantity(); }
+    void Fill(Quantity quantity) {
+        if (quantity > getRemainingQuantity())
+            throw std::logic_error(std::format("Order ({}) cannot be filled for more than its remaining quantity.", getOrderId()));
+        
+        remainingQuantity_ -= quantity;
+    }
+
+    private:
+        OrderType orderType_;
+        OrderId orderId_;
+        Side side_;
+        Price price_;
+        Quantity initialQuantity_;
+        Quantity remainingQuantity_;
+        
+};
+
+
 int main() {
 
     return 0;
