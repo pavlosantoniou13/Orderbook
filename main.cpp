5ed286e51e5f0f2a39f21e21fe9ccde75bd33e46
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-09-25 12:25:53 +0200

    add OrderBookLevelInfos class, LevelInfo data model

--- main.cpp
+++ main.cpp
@@ -33,6 +33,30 @@ using Price = std::int32_t;
 using Quantity = std::uint32_t;
 using OrderId = std::uint32_t;
 
+struct LevelInfo
+{
+    Price price_;
+    Quantity quantity_;
+};
+
+using LevelInfos = std::vector<LevelInfo>;
+
+class OrderBookLevelInfos
+{
+    public:
+        OrderBookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
+        : bids_{ bids }
+        , asks_{ asks }
+        { }
+
+        const LevelInfos& getBids() const {return bids_; }
+        const LevelInfos& getAsks() const {return asks_; }
+
+    private: 
+        LevelInfos bids_;
+        LevelInfos asks_;
+};
+
 int main() {
 
     return 0;
