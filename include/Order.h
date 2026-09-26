9c38b707e8c8bd1eb068de7bfdc37b3bc95e37ec
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-09-26 16:53:06 +0200

    Update Order.h
    New constructor for Market

--- include/Order.h
+++ include/Order.h
@@ -12,6 +12,10 @@ public:
         : orderType_{ orderType }, orderId_{ orderId }, side_{ side }
         , price_{ price }, initialQuantity_{ quantity }, remainingQuantity_{ quantity }
     { }
+
+    Order(OrderId orderId, Side side, Quantity quantity)
+        : Order(OrderType::Market, orderId, side, Constants::InvalidPrice, quantity)
+        { }
     
     OrderId getOrderId() const { return orderId_; }
     Side getSide() const { return side_; }
